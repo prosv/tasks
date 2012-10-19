0ca123aec0810ebce981d3d0c403e7b0816f16ee
Prosv <prosvirnina.mk@gmail.com>
2012-10-19 15:58:55 +0400

    fixed defects

--- levenshtain/main.cpp
+++ levenshtain/main.cpp
@@ -13,20 +13,26 @@ int levenshtain (char s1[], char s2[])
         const int len1 = str_length(s1);
         const int len2 = str_length(s2);
         int *matr = new int [(len1+1)*(len2+1)];
-        for(int i = 0; i <= len1; ++i)
+        for(int i = 0; i <= len1; ++i){
             matr[i] = i;
-        for(int i = 1; i <= len2; ++i)
+        }
+        for(int i = 1; i <= len2; ++i){
             matr[i*(len1+1)] = i;
-        for(int i = 1; i <= len2; ++i)
-                for(int j = 1; j <= len1; ++j)
+        }
+        bool bl;
+        for(int i = 1; i <= len2; ++i){
+                for(int j = 1; j <= len1; ++j){
+                      bl = s1[i - 1] == s2[j - 1] ? 0 : 1;
                       matr[i*(len1+1)+j] = std::min( std::min(matr[(i - 1)*(len1+1)+j] + 1
                                                    , matr[i*(len1+1)+j-1] + 1)
-                                                   , matr[(i - 1)*(len1+1)+j-1] + (s1[i - 1] == s2[j - 1] ? 0 : 1) );
+                                                   , matr[(i - 1)*(len1+1)+j-1] + bl );
+                }
+        }
         return matr[(len1+1)*(len2+1)-1];
 }
 int main(int arg, char* argv[])
 {
-    std::cout << levenshtain ("1123ыва","123ыав");
+    std::cout << levenshtain ("123ytr1","123tyr");
     std::cin.sync();
     std::cin.get();
     return 0;
